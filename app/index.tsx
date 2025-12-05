98f4c8df79a23d1586d7fdcef6581c2b781adbb8
Mcky0101 <camaramacky73@gmail.com>
2025-12-05 23:39:22 +0000

    ras

--- app/index.tsx
+++ app/index.tsx
@@ -5,6 +5,7 @@ export default function LoginPage() {
   return (
     <SafeAreaView className="flex-1 justify-center items-center">
       <Text className="text-rose-500 text-xl font-bold">salut la plus belle Reine Lalli</Text>
+      <Text className="text-rose-800 text-xl font-bold">La Reine des neige</Text>
     </SafeAreaView>
   )
 }
